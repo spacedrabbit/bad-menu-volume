8c16d05642dc18eb4b3a09c34462349a1763a0c1
Louis Tur <louis.tur@gmail.com>
2017-06-10 16:37:58 -0400

    base commit

---
+++ bad-menu-volume/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  bad-menu-volume
+//
+//  Created by Louis Tur on 6/10/17.
+//  Copyright © 2017 catthoughts. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ bad-menu-volume/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  bad-menu-volume
+//
+//  Created by Louis Tur on 6/10/17.
+//  Copyright © 2017 catthoughts. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
