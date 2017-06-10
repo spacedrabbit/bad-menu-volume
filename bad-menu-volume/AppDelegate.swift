f050dd6dcf5005aeaa8aa07aed47c9f3fbbdcc86
Louis Tur <louis.tur@gmail.com>
2017-06-10 18:00:24 -0400

    fixes ridiculous missing targets, removes storyboard

--- bad-menu-volume/AppDelegate.swift
+++ bad-menu-volume/AppDelegate.swift
@@ -10,17 +10,37 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-  func applicationDidFinishLaunching(_ aNotification: Notification) {
-    // Insert code here to initialize your application
+  
+  @IBOutlet weak var statusMenu: NSMenu!
+  @IBAction func didPressQuit(_ sender: NSMenuItem) {
+    NSApplication.shared().terminate(self)
   }
+  
+  let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+  
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    
+    statusItem.title = "WeatherBar"
+    statusItem.menu = statusMenu
+//    
+//    if let button = statusItem.button {
+//      button.image = NSImage(named: "StatusBarButtonImage")
+//      button.action = Selector(("printQuote:"))
+//    }
+//    
+//    
+  }
+  
 
   func applicationWillTerminate(_ aNotification: Notification) {
     // Insert code here to tear down your application
   }
 
-
+  func printQuote(sender: AnyObject) {
+    let quoteText = "Never put off until tomorrow what you can do the day after tomorrow"
+    let quoteAuthor = "Mark Twain"
+    
+    print("\(quoteText) - \(quoteAuthor)")
+  }
 }
 
